d93afa9151590613699625f232e77d219fd4ad14
Lashod Jones <lashodj@gmail.com>
2024-06-06 01:06:11 -0400

    working dice game completed

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var diceRoll1 = "images/dice" + randomNumber1 + ".png"
-var diceRoll2 = "images/Player2dice" + randomNumber2 + ".png"
+ // Generate random numbers for dice rolls
+var randomNumber1
+var randomNumber2
+
+
 var dice1 = document.querySelectorAll("img")[0]
 var dice2 = document.querySelectorAll("img")[1]
-var rollDice = document.querySelector("button")
 
-    dice1.setAttribute("src", diceRoll1);
-    dice2.setAttribute("src" ,diceRoll2);
+var resultDisplay = document.querySelector("h1");
+
+document.querySelector("#dice-button").addEventListener("click" , rollDice)
+
+function rollDice(event){
+
+    randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+     // Reset the previous game results
+     document.querySelector("h1").innerHTML = "";
+
+    dice1.setAttribute("src", "images/dice" + randomNumber1 + ".png" );
+    dice2.setAttribute("src" , "images/Player2dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML="🚀Player 1 Wins🥺"
+    } else if (randomNumber1 < randomNumber2){
+         document.querySelector("h1").innerHTML="🥺Player 2 Wins🚀"
+    } else {
+        document.querySelector("h1").innerHTML= "🎲Draw🎲"
+    }
+
+
+}
+
+
+
+ 
+
+
 
 
  
